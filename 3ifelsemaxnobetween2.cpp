5ef04545b3b79a0db2279cab01232f9d7a0e1822
SNEHA PANKHI <snehapankhi105@gmail.com>
2024-08-16 17:52:46 +0530

    Add files via upload

---
+++ 3ifelsemaxnobetween2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+//ifelse==maximum number between 2
+int main()
+{
+    int num1,num2;
+    cin>>num1>>num2;
+    if(num1>num2)
+    {
+        cout<<"number1 is maximum "<<num1<<endl;
+    }
+    else
+    {
+        cout<<"number 2 is maximum "<<num2<<endl;
+    }
+    return 0;
+
+}
